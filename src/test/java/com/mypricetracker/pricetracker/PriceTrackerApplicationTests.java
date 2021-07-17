81a58e239aadb7988546016b2ca67f01e2163696
Dudek-exe <marek.patryk.dudzinski@gmail.com>
2021-07-17 09:44:29 +0200

    First commit

---
+++ src/test/java/com/mypricetracker/pricetracker/PriceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypricetracker.pricetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
