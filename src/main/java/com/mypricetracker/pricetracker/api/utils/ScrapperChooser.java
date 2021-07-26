f177451c1f45865bf5ac591c203ab8c0058b0532
Dudek-exe <marek.patryk.dudzinski@gmail.com>
2021-07-26 22:58:31 +0200

    Last refactor for today

--- src/main/java/com/mypricetracker/pricetracker/api/utils/ScrapperChooser.java
+++ src/main/java/com/mypricetracker/pricetracker/api/utils/ScrapperChooser.java
@@ -1,6 +1,6 @@
 package com.mypricetracker.pricetracker.api.utils;
 
-import com.mypricetracker.pricetracker.exception.UnreachableException;
+import com.mypricetracker.pricetracker.exception.NoSuchScrapperEnumTypeException;
 import com.mypricetracker.pricetracker.scrapper.Scrapper;
 import com.mypricetracker.pricetracker.scrapper.ScrapperContainer;
 import com.mypricetracker.pricetracker.scrapper.ScrapperTypeEnum;
@@ -20,16 +20,15 @@ public class ScrapperChooser {
     public Scrapper extractScrapperFromUrl(String url) {
         url = url.replace("https://www.", "").replace("-", "");
         String scrapperTypeFromUrl = url.substring(0, 10).toUpperCase(Locale.ROOT);
-        return scrapperContainer.getScrapperMap().get(ScrapperTypeEnum.from(scrapperTypeFromUrl));
+        return scrapperContainer.getScrapperMap().get(ScrapperTypeEnum.from(chooseScrapperFromUrl(scrapperTypeFromUrl)));
     }
 
     //TODO add more cases
-    public Scrapper chooseScrapper(ScrapperTypeEnum scrapperTypeEnum) {
-        switch (scrapperTypeEnum) {
-            case XKOM:
-                return scrapperContainer.getScrapperMap().get(scrapperTypeEnum);
-        }
-        throw new UnreachableException("Something went wrong");
+    private String chooseScrapperFromUrl(String scrapperTypeFromUrl) {
+        if (scrapperTypeFromUrl.contains(ScrapperTypeEnum.XKOM.getScrapperType()))
+            return ScrapperTypeEnum.XKOM.getScrapperType();
+
+        throw new NoSuchScrapperEnumTypeException("Could not create Scrapper Type of: " + scrapperTypeFromUrl);
     }
 
 
